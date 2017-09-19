8603300cd0b144dbd69418891e206000a06ca5f8
Hwayong Jang <idgaid92320@naver.com>
2017-09-19 21:01:42 +0900

    New Website Text Crawler

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,22 +1,31 @@
-input_file = open("text.txt", "r")
+import urllib.request
 
-r = input_file.read()
+url = "http://post.naver.com/viewer/postView.nhn?volumeNo=9577951&memberNo=36405506&mainMenu=SCIENCE"
 
-words = r.replace('#',' ').replace('(',' ').replace(')',' ').replace('/',' ').replace(',',' ').replace('.',' ').replace('?',' ').replace('-',' ').replace('!',' ').split()
-num = len(words)
+f = urllib.request.urlopen(url)
+data = f.read().decode('utf-8')
 
-d = {}
+begin = data.find("<span>일식은 연구자들에게",1)
+end = data.find("확인하고자 하고 있다")
+end += len("확인하고자 하고 있다")
 
-for i in range(num):
-    k = words[i]
-    if k in d.keys():
-        d[words[i]] += 1
-    else :
-        d[words[i]] = 1
+print("total=", len(data))
+print("begin=", begin)
+print("length=", end-begin, '\n')
 
-sort_1 = sorted(d.items())
-sort_2 = sorted(sort_1, key = lambda x:x[1], reverse=True)
+speech = data[begin:end]
+re_speech = speech.replace('</span>',' ').replace('<span lang="EN-US">',' ').replace('<br>',' ').replace('<span>',' ').replace('&nbsp',' ').replace('(',' ').replace(')',' ').replace('.',' ').replace(';',' ').replace("\'",' ').replace('\"',' ').replace(',',' ')
+
+speech = re_speech.split()
+
+analyze = {}
+
+for word in speech:
+    analyze[word] = analyze.get(word, 0) + 1
+
+flist_1 = sorted(analyze.items())
+flist_2 = sorted(analyze.items(), key=lambda kv: kv[1], reverse=True)
 
 
-for key in sort_2:
+for key in flist_2:
     print (key)
